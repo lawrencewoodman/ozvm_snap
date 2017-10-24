8e0df24bacd702bd16d88a1cadd411f46fcdffbf
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-10-24 07:27:49 +0100

    At this point it partially works.
    It does complain about /usr/bin/xprop, but does bring up the emulator
    and finds the roms.

--- launcher.sh
+++ launcher.sh
@@ -9,4 +9,27 @@ export XDG_DATA_HOME=$SNAP/usr/share
 export FONTCONFIG_PATH=$SNAP/etc/fonts/config.d
 export FONTCONFIG_FILE=$SNAP/etc/fonts/fonts.conf
 
+# mount is needed because jar tries to used hard coded path for
+# /usr/bin/xprop
+#export SUDO_ASKPASS=$SNAP/usr/lib/ssh/x11-ssh-askpass
+#sudo -A mount --bind /usr/bin $SNAP/usr/bin
+#sudo -A ln -s /usr/bin/xprop $SNAP/usr/bin/xprop
+
+
+echo ls $SNAP/usr/ozvm
+ls $SNAP/usr/ozvm
+echo ls $SNAP/usr/ozvm/roms
+ls $SNAP/usr/ozvm/roms
+
+echo ls $SNAP_USER_DATA
+ls
+
+echo ls $SNAP/usr/bin
+ls $SNAP/usr/bin
+
+echo ls /usr/bin
+ls /usr/bin
+
 java -jar -Duser.home=$SNAP_USER_DATA $SNAP/usr/ozvm/z88.jar
+
+#sudo -A umount $SNAP/usr/bin
